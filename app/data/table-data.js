c71abf7112ebbdd2685dae210276ff0bd0fac31c
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-03-02 10:48:43 -0500

    initial checkin

---
+++ app/data/table-data.js
@@ -0,0 +1,18 @@
+var tableArray = [
+	{
+		customerName: "Asha",
+		customerEmail: "asha@gmail.com",
+		customerID: "asha1",
+		phoneNumber: "999-287-0387",
+
+	},
+	{
+		customerName: "Usha",
+		customerEmail: "usha@gmail.com",
+		customerID: "usha2",
+		phoneNumber: "999-287-0387",
+
+	}
+];
+ 
+module.exports = tableArray; 
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,18 @@
+var waitingArray = [
+	{
+		customerName: "Cathy",
+		customerEmail: "cathy@gmail.com",
+		phoneNumber: "979-587-0887",
+		customerID: "cathy3"
+	},
+	{
+		customerName: "Kevin",
+		customerEmail: "kevin@gmail.com",
+		phoneNumber: "979-237-0347",
+		customerID: "kevin4"
+	}
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require. 
+module.exports = waitingArray;
